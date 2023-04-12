0f3c7363577ba461c357f40d6e433d13fcad8ea9
Rene Tran-Guillot <10559449+reneguillot@users.noreply.github.com>
2023-04-12 22:29:36 +0200

    Update chat-search.js

--- chat-search.js
+++ chat-search.js
@@ -1,7 +1,7 @@
 const platformClient = require("platformClient");
 const clientId = "ca9b9d2c-978e-4da4-a731-f4ede41258cc";
 const redirectUri =
-  "https://reneguillot.github.io/GSYS-chat-search";
+  "https://reneguillot.github.io/GSYS-chat-search/";
 const client = platformClient.ApiClient.instance;
 var md = window.markdownit();
 var state = "";
